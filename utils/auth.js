d5697e1a5e14cd95b8453b438d80a707ef652f5b
etozhefrank <51893554+etozhefrank@users.noreply.github.com>
2021-07-09 19:55:04 +0300

    Delete utils directory

--- utils/auth.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-
-const hashPassword = (plainText) => {
-  return crypto.createHmac(process.env.algo, process.env.secret)
-    .update(plainText)
-    .digest('hex');
-}
-
-module.exports = { hashPassword };
